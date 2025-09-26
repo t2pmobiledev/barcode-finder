0f169737c6e409f0114119a1c46fd7147a49c403
chettha pilarit <chettha_pil@t2pco.com>
2025-09-26 23:43:16 +0700

    16 KB

--- android/src/main/java/br/com/popcode/barcode_finder/ReadBarcodeFromFile.java
+++ android/src/main/java/br/com/popcode/barcode_finder/ReadBarcodeFromFile.java
@@ -18,8 +18,8 @@ import com.google.zxing.RGBLuminanceSource;
 import com.google.zxing.Reader;
 import com.google.zxing.Result;
 import com.google.zxing.common.HybridBinarizer;
-import com.shockwave.pdfium.PdfDocument;
-import com.shockwave.pdfium.PdfiumCore;
+//import com.shockwave.pdfium.PdfDocument;
+//import com.shockwave.pdfium.PdfiumCore;
 
 import java.util.ArrayList;
 import java.util.EnumMap;
@@ -64,7 +64,8 @@ public class ReadBarcodeFromFile extends AsyncTask<Void, Void, String> {
             while (tryNumber <= NUMBER_OF_ATTEMPTS && !outOfMemoryError) {
                 Bitmap bitmap;
                 if (entryType == EntryType.PDF) {
-                    bitmap = generateImageFromPdf(filePath, 0, tryNumber);
+//                    bitmap = generateImageFromPdf(filePath, 0, tryNumber);
+                    bitmap = null;
                 } else {
                     bitmap = BitmapFactory.decodeFile(filePath.getPath());
                     bitmap = resizeImage(bitmap, tryNumber);
@@ -94,29 +95,29 @@ public class ReadBarcodeFromFile extends AsyncTask<Void, Void, String> {
         }
     }
 
-    private Bitmap generateImageFromPdf(Uri assetFileName, int numeroPagina, int tryNumber) {
-        PdfiumCore pdfiumCore = new PdfiumCore(context);
-        try {
-            ParcelFileDescriptor parcelFileDescriptor;
-            ContentResolver contentResolver = context.getApplicationContext().getContentResolver();
-            parcelFileDescriptor = contentResolver.openFileDescriptor(assetFileName, "r", null);
-            PdfDocument pdfDocument = pdfiumCore.newDocument(parcelFileDescriptor);
-            pdfiumCore.openPage(pdfDocument, numeroPagina);
-            int width = pdfiumCore.getPageWidthPoint(pdfDocument, numeroPagina) * tryNumber;
-            int height = pdfiumCore.getPageHeightPoint(pdfDocument, numeroPagina) * tryNumber;
-            Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-            pdfiumCore.renderPageBitmap(pdfDocument, bmp, numeroPagina, 0, 0, width, height);
-            pdfiumCore.closeDocument(pdfDocument);
-            return bmp;
-        } catch (OutOfMemoryError e) {
-            Log.e("BarcodeTest", "Error Out of memory", e);
-            outOfMemoryError = true;
-            return null;
-        } catch (Exception e) {
-            Log.e("Bitmap PDF", e.getMessage());
-        }
-        return null;
-    }
+//    private Bitmap generateImageFromPdf(Uri assetFileName, int numeroPagina, int tryNumber) {
+//        PdfiumCore pdfiumCore = new PdfiumCore(context);
+//        try {
+//            ParcelFileDescriptor parcelFileDescriptor;
+//            ContentResolver contentResolver = context.getApplicationContext().getContentResolver();
+//            parcelFileDescriptor = contentResolver.openFileDescriptor(assetFileName, "r", null);
+//            PdfDocument pdfDocument = pdfiumCore.newDocument(parcelFileDescriptor);
+//            pdfiumCore.openPage(pdfDocument, numeroPagina);
+//            int width = pdfiumCore.getPageWidthPoint(pdfDocument, numeroPagina) * tryNumber;
+//            int height = pdfiumCore.getPageHeightPoint(pdfDocument, numeroPagina) * tryNumber;
+//            Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+//            pdfiumCore.renderPageBitmap(pdfDocument, bmp, numeroPagina, 0, 0, width, height);
+//            pdfiumCore.closeDocument(pdfDocument);
+//            return bmp;
+//        } catch (OutOfMemoryError e) {
+//            Log.e("BarcodeTest", "Error Out of memory", e);
+//            outOfMemoryError = true;
+//            return null;
+//        } catch (Exception e) {
+//            Log.e("Bitmap PDF", e.getMessage());
+//        }
+//        return null;
+//    }
 
     private String scanImage(Bitmap bMap, Reader reader) {
         String contents = null;
